abf926edc1f9b087c92e99ea0bc3e41fc31ed05b
Dirac <109837740+Dirac2022@users.noreply.github.com>
2023-08-28 08:51:29 -0500

    pruebaEntrada.java

--- pruebaEntrada.java
+++ pruebaEntrada.java
@@ -12,6 +12,8 @@ obtenerEdad (2pts)
  Método de clase: calcularEdadPromedio (3pts)
  Programa que registre tres personas y calcule su edad promedio. (4)
 */
+
+import java.util.Scanner;
 public class Persona {
     
     String nombre;
@@ -66,15 +68,51 @@ public class Persona {
     
     
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
         Persona persona1 = new Persona();
-        Persona persona2 = new Persona("Ivan", 16, "Masculino");
-        Persona persona3 = new Persona("Andre", 15, "Masculino");
+        Persona persona2 = new Persona();
+        Persona persona3 = new Persona();
         
-        persona1.setearNombre("Mitchel");
-        persona1.setearEdad(21);
+        String nombre, genero;
+        int edad;
+        
+        System.out.println("Ingrese datos de primera persona");
+        System.out.println("Nombre: ");
+        nombre = sc.nextLine();
+        persona1.setearNombre(nombre);
+        System.out.println("Edad:");
+        edad = sc.nextInt();
+        persona1.setearEdad(edad);
+        sc.nextLine();
+        System.out.println("Genero:");
+        persona1.genero = sc.nextLine();
+        
+        System.out.println("Ingrese datos de segunda persona");
+        System.out.println("Nombre: ");
+        nombre = sc.nextLine();
+        persona2.setearNombre(nombre);
+        System.out.println("Edad:");
+        edad = sc.nextInt();
+        persona2.setearEdad(edad);
+        sc.nextLine();
+        System.out.println("Genero:");
+        persona2.genero = sc.nextLine();
+        
+        System.out.println("Ingrese datos de tercera persona");
+        System.out.println("Nombre: ");
+        nombre = sc.nextLine();
+        persona3.setearNombre(nombre);
+        System.out.println("Edad:");
+        edad = sc.nextInt();
+        persona3.setearEdad(edad);
+        sc.nextLine();
+        System.out.println("Genero:");
+        persona3.genero = sc.nextLine();
         
         double edadPromedio = Persona.calcularEdadPromedio();
         System.out.printf("La edad acumulada de las 3 personas es: %.2f",edadPromedio);
               
     }
+    
+}
